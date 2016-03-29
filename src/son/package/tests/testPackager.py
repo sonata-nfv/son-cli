2353158e616f7f7b35b3b8855e4be899ff6757ae
lconceicao <lconceicao@ubiwhere.com>
2016-03-29 15:02:06 +0100

    Included tests to validate the remote load of schemas.

--- src/son/package/tests/testPackager.py
+++ src/son/package/tests/testPackager.py
@@ -1,8 +1,7 @@
 import unittest
 
-import yaml
-
-from son.package.package import Packager, load_local_schema
+from urllib.error import HTTPError
+from son.package.package import Packager, load_local_schema, load_remote_schema
 
 
 class PDTester(unittest.TestCase):
@@ -34,16 +33,30 @@ class PDTester(unittest.TestCase):
         self.assertEqual(gsd, False)
 
 
-class LoadSchemaTestes(unittest.TestCase):
+class LoadSchemaTests(unittest.TestCase):
 
-    def test_load_invalid_template(self):
+    def test_load_invalid_local_template(self):
         """Test if the load schema is loading only available templates"""
         self.assertRaises(FileNotFoundError, load_local_schema, "test")
 
-    def test_load_valid_schema(self):
+    def test_load_valid_local_schema(self):
         """ Test if the load schema is correctly loading the templates """
         schema = load_local_schema(Packager.local_schemas["PD"])
         self.assertIsInstance(schema, dict)
+
+    def test_load_invalid_remote_template_unavailable(self):
+        """ Test if it raises a HTTP error with a valid but unavailable schema URL """
+        self.assertRaises(HTTPError, load_remote_schema, "http://somerandomurl.com/artifact.yml")
+
+    def test_load_invalid_remote_template_invalid(self):
+        """ Test if it raises an error with an invalid schema URL """
+        self.assertRaises(ValueError, load_remote_schema, "some.incorrect/..url")
+
+    def test_load_valid_remote_schema(self):
+        """ Test if the load_remote_schema is retrieving and loading the templates correctly """
+        schema = load_remote_schema(Packager.remote_schemas["PD"])
+        self.assertIsInstance(schema, dict)
+
 
 if __name__ == '__main__':
     unittest.main()
