8eff22d755d2476bdb365a6a63246b9c43a1f458
Luis Conceicao <lconceicao@ubiwhere.com>
2016-05-10 10:27:39 +0100

    Created a test for package generation. It ensures that the package file is created with the correct name at the correct location.

--- src/son/package/tests/test_unit_Packager.py
+++ src/son/package/tests/test_unit_Packager.py
@@ -1,13 +1,16 @@
 import unittest
 from unittest.mock import patch
 from unittest.mock import Mock
+from unittest import mock
 from son.package.package import Packager
 from son.workspace.workspace import Workspace
-
+import os
 
 class UnitCreatePackageTests(unittest.TestCase):
 
-    def test_generate_package(self):
+    @patch('son.package.package.os.path.join')
+    @patch('son.package.package.zipfile')
+    def test_generate_package(self, m_zipfile, m_join):
         """
         Ensures that a package file is created with correct name and location
         """
@@ -15,8 +18,24 @@ class UnitCreatePackageTests(unittest.TestCase):
         workspace = Workspace("ws/root", ws_name="ws_test", log_level='debug')
 
         # Instantiate a Packager instance
-        packager = Packager("prj/path", workspace, generate_pd=False)
+        packager = Packager("prj/path", workspace, generate_pd=False, dst_path="dst/path")
+        packager._package_descriptor = True
 
+        # Prepare mocks
+        context_manager_mock = Mock()
+        m_zipfile.ZipFile.return_value = context_manager_mock
+        enter_mock = Mock()
+        exit_mock = Mock()
+        setattr(context_manager_mock, '__enter__', enter_mock)
+        setattr(context_manager_mock, '__exit__', exit_mock)
+
+        # execute
+        packager.generate_package("package_name")
+
+        # make assertions
+        self.assertEqual(m_join.call_args_list[-1], mock.call('dst/path', 'package_name.son'))
+
+    
 
 
     @patch('son.package.package.yaml')
