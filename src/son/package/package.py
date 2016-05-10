21143e03e2039677b157597fb4c2bf866b66f87b
Luis Conceicao <lconceicao@ubiwhere.com>
2016-05-10 14:47:10 +0100

    son.package should perform exit(2), instead of only printing to stderr, when packaging fails.

--- src/son/package/package.py
+++ src/son/package/package.py
@@ -462,7 +462,7 @@ class Packager(object):
         # Validate all needed information
         if not self._package_descriptor:
             log.error("Missing package descriptor. Failed to generate package.")
-            return
+            exit(1)
 
         # Generate package file
         zip_name = os.path.join(self._dst_path, name + '.son')
