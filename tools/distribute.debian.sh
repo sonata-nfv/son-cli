8d0fe224b40b8a14e655346f7c157410347680a0
Luis Conceicao <lconceicao@ubiwhere.com>
2016-06-29 13:12:35 +0100

    corrected bug

--- tools/distribute.debian.sh
+++ tools/distribute.debian.sh
@@ -2,9 +2,13 @@
 
 # this script is to be run at root of "son-cli" dir
 
+set -xe
+
 # install py2deb tool
-pip3 install py2deb
-sudo apt-get install -y dpkg-dev fakeroot
+sudo apt-get update
+sudo apt-get install -y python-pip dpkg-dev fakeroot
+pip install py2deb
+
 
 # package project and dependencies into debs
 py2deb -r sonata-dist-deb -- .
