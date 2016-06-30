0baf8e94e36ac21187ec2db9dacc00db355050ea
Luis Conceicao <lconceicao@ubiwhere.com>
2016-06-30 09:29:23 +0100

    Added run docker container to serve debian repository.

--- tools/distribute.debian.sh
+++ tools/distribute.debian.sh
@@ -11,6 +11,21 @@ pip3 install py2deb
 
 
 # package project and dependencies into debs
-mkdir -p sonata-dist-deb
-py2deb -r sonata-dist-deb -- .
+mkdir -p debs
+py2deb -r debs --no-name-prefix=sonata-cli .
 
+export DOCKER_HOST="tcp://registry.sonata-nfv.eu:2375"
+
+docker run debifymod -d -e URI=http://registry.sonata-nfv.eu:8080 \
+ -e KEYSERVER=keyserver.ubuntu.com -e APTLY_ARCHITECTURES="i386,amd64" \
+ --entrypoint=/bin/bash -p 8080:8080 debifymod
+
+docker restart debifymod
+
+docker cp debs debifymod:/
+
+docker exec debifymod debify.sh
+sleep 5
+docker exec debifymod aptly serve
+
+export DOCKER_HOST="unix:///var/run/docker.sock"
