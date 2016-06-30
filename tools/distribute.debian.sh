5b2d57df43a776c8a80db9c3d130e2078c332e7b
Luis Conceicao <lconceicao@ubiwhere.com>
2016-06-30 11:38:58 +0100

    Corrected bugs in debian distribution script Added log messages

--- tools/distribute.debian.sh
+++ tools/distribute.debian.sh
@@ -2,30 +2,65 @@
 
 # this script is to be run at root of "son-cli" dir
 
-set -xe
+set -e
+
+echo ""
+echo "==== Build packages and instantiate debian repository ===="
+echo ""
 
 # install py2deb tool
+echo "-> Preparing environment"
 sudo apt-get update
 sudo apt-get install -y python3-pip dpkg-dev fakeroot
-pip3 install py2deb
+sudo pip3 install py2deb
 
 
 # package project and dependencies into debs
+echo "-> Creating deb packages"
 mkdir -p debs
 py2deb -r debs --no-name-prefix=sonata-cli .
 
+
+echo "-> Running repository container"
+# point to remote docker daemon
 export DOCKER_HOST="tcp://registry.sonata-nfv.eu:2375"
 
-docker run debifymod -d -e URI=http://registry.sonata-nfv.eu:8080 \
+# check if container is running and stop/remove it
+CONTAINER="debifymod"
+RUNNING=$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
+
+if [ $? -eq 1 ]; then
+  echo "'$CONTAINER' does not exist."
+
+elif [ "$RUNNING" == "false" ]; then
+  echo "'$CONTAINER' is not running. Just remove it."
+  docker rm "$CONTAINER"
+
+else
+  echo "$CONTAINER is running. Stop and remove it."
+  docker stop "$CONTAINER"
+  docker rm "$CONTAINER"
+fi
+
+
+# run server repository container 
+docker run --name=debifymod -di -e URI=http://registry.sonata-nfv.eu:8080 \
  -e KEYSERVER=keyserver.ubuntu.com -e APTLY_ARCHITECTURES="i386,amd64" \
  --entrypoint=/bin/bash -p 8080:8080 debifymod
 
-docker restart debifymod
-
+# copy newly generated deb packages to it and create repository
 docker cp debs debifymod:/
-
-docker exec debifymod debify.sh
+docker exec -d debifymod sh /debify.sh
 sleep 5
-docker exec debifymod aptly serve
+docker exec -d debifymod ln -s /repo /root/.aptly/public
+docker exec -d debifymod aptly serve
 
+echo "######"
+docker exec debifymod cat repo/go
+echo "######"
+echo "To install SONATA CLI tools run 'apt-get install sonata-cli'"
+
+# restore address to local docker daemon
 export DOCKER_HOST="unix:///var/run/docker.sock"
+
+echo "-> Done."
