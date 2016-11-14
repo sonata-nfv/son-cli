d6f566e8e2a4119109171e74e17b151090f03699
Manuel Peuster <manuel@peuster.de>
2016-11-14 17:46:29 +0100

    son-profile: First part of service generation.

--- src/son/profile/experiment.py
+++ src/son/profile/experiment.py
@@ -25,9 +25,9 @@
 # partner consortium (www.sonata-nfv.eu).
 
 import logging
-import coloredlogs
-import itertools as it
 from son.profile.macro import rewrite_parameter_macros_to_lists
+from son.profile.helper import compute_cartesian_product
+from son.profile.sonpkg import SonataServicePackage
 LOG = logging.getLogger(__name__)
 
 
@@ -52,14 +52,15 @@ class Experiment(object):
         parameter_dict = self._get_configuration_space_as_dict()
         # print(parameter_dict)
         # explore entire parameter space by calculating the Cartesian product over the given dict
-        parameter_space_list = Experiment._compute_cartesian_product(parameter_dict)
+        parameter_space_list = compute_cartesian_product(parameter_dict)
         # create a run configuration object for each calculated configuration to test
         for i in range(0, len(parameter_space_list)):
             self.run_configurations.append(RunConfiguration(i, parameter_space_list[i]))
-        LOG.info("Populated experiment: %r with %d configurations to test." % (self.name, len(self.run_configurations)))
+        LOG.info("Populated experiment specifications: %r with %d configurations to test." % (self.name, len(self.run_configurations)))
 
     def _get_configuration_space_as_dict(self):
         """
+        Create a flat dictionary with configuration lists to be tested for each configuration parameter.
         Output: dict
         {"function1:parameter1" : [0.1, 0.2, ...],
          "functionN:parameterN" : [0.1, ...],
@@ -80,23 +81,33 @@ class Experiment(object):
         r["repetition"] = list(range(0, self.repetitions))
         return r
 
-    @staticmethod
-    def _compute_cartesian_product(p_dict):
+    def generate_sonata_services(self, son_pkg_input):
         """
-        Compute Cartesian product on parameter dict:
-        In:
-            {"number": [1,2,3], "color": ["orange","blue"] }
-        Out:
-            [ {"number": 1, "color": "orange"},
-              {"number": 1, "color": "blue"},
-              {"number": 2, "color": "orange"},
-              {"number": 2, "color": "blue"},
-              {"number": 3, "color": "orange"},
-              {"number": 3, "color": "blue"}
-            ]
+        Create a SONATA service for each configuration to be tested.
+        :param son_pkg_input: base service to be used
+        :return: list of services
         """
-        p_names = sorted(p_dict)
-        return [dict(zip(p_names, prod)) for prod in it.product(*(p_dict[n] for n in p_names))]
+        services = list()
+        for r in self.run_configurations:
+            s = son_pkg_input.copy()
+            s.annotate(r)
+            self.modify_nsd(s, r)
+            self.modify_vnfds(s, r)
+            services.append(s)
+        return services
+
+    def modify_nsd(self, service, run_cfg):
+        """
+        Abstract: Needs to be overwritten.
+        :param service: service to be modified
+        :param run_cfg: run configuration to be applied
+        :return:
+        """
+        pass
+
+    def modify_vnfds(self, service, run_cfg):
+        # TODO: apply resource limitations to VNFDs
+        pass
 
 
 class ServiceExperiment(Experiment):
@@ -105,12 +116,32 @@ class ServiceExperiment(Experiment):
         super().__init__(definition)
         LOG.debug("Created service experiment: %r" % self.name)
 
+    def modify_nsd(self, service, run_cfg):
+        """
+        Add measurement point (MP) VNFs to service graph of original NSD.
+        :param service: service to be modified
+        :param run_cfg: run configuration to be applied
+        :return:
+        """
+        # TODO: implement
+        pass
+
 
 class FunctionExperiment(Experiment):
 
     def __init__(self, definition):
         super().__init__(definition)
         LOG.debug("Created function experiment: %r" % self.name)
+
+    def modify_nsd(self, service, run_cfg):
+        """
+        Create a new NSD which is dedicated to test a single VNF and add measurement point (MP) VNFs to service graph.
+        :param service: service to be modified
+        :param run_cfg: run configuration to be applied
+        :return:
+        """
+        # TODO: implement
+        pass
 
 
 class RunConfiguration(object):
