e0d41ea9070c3a638a34a0bfc073928ecbd56d4e
Luís Conceição <lconceicao@ubiwhere.com>
2017-04-21 16:52:06 +0100

    - added common function to process different validation requests - redefined structure of cached resources

--- src/son/validate/api/api.py
+++ src/son/validate/api/api.py
@@ -20,6 +20,9 @@ app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 cache = Cache(app, config={'CACHE_TYPE': 'redis'})
 
+# keep temporary request errors
+req_errors = []
+
 
 def initialize():
     cache.clear()
@@ -45,10 +48,23 @@ def add_artifact_root():
     return artifact_root
 
 
-def set_resource(key, resource):
+def set_resource(key, type=None, result=None, topology=None, fwgraph=None):
+    assert type or result or topology or fwgraph
+
     log.debug("Caching resource '{0}'".format(key))
     resources = cache.get('resources')
-    resources[key] = resource
+    if key not in resources.keys():
+        resources[key] = dict()
+
+    if type:
+        resources[key]['type'] = type
+    if result:
+        resources[key]['result'] = result
+    if topology:
+        resources[key]['topology'] = topology
+    if fwgraph:
+        resources[key]['fwgraph'] = fwgraph
+
     cache.set('resources', resources)
 
 
@@ -58,13 +74,12 @@ def get_resource(key):
     return cache.get('resources')[key]
 
 
-
 def get_resource_key(path):
     print(os.path.abspath(path))
     return generate_hash(os.path.abspath(path))
 
 
-def preprocess_request():
+def process_request():
     source = request.form['source']
 
     if source == 'local' and 'path' in request.form:
@@ -79,10 +94,51 @@ def preprocess_request():
         path = get_file(request.files['file'])
 
     else:
-        print('error')
+        req_errors.append('Invalid source, path or file parameters')
         return
 
     return path
+
+
+def _validate_object(object_type):
+    assert object_type == 'project' or object_type == 'package' or \
+           object_type == 'service' or object_type == 'function'
+
+    path = process_request()
+    if not path:
+        return render_errors(), 400
+
+    log.info("Validating {0} '{1}'".format(object_type, path))
+    key = get_resource_key(path)
+    log.debug("MD5 hash key: '{}'".format(key))
+
+    resource = get_resource(key)
+    if resource and resource['type'] == object_type:
+        log.debug("Returning cached result for '{0}'".format(key))
+        return resource['result']
+
+    validator = Validator()
+    validator.configure(syntax=request.form['syntax'] or False,
+                        integrity=request.form['integrity'] or False,
+                        topology=request.form['topology'] or False,
+                        debug=app.debug)
+
+    val_function = getattr(validator, 'validate_' + object_type)
+    result = val_function(path)
+    print_result(validator, result)
+    json_result = generate_result(validator)
+    # todo: missing topology and fwgraphs
+    set_resource(key, type=object_type, result=json_result)
+
+    return json_result
+
+
+def render_errors():
+    error_str = ''
+    for error in req_errors:
+        error_str += error + '\n'
+    req_errors.clear()
+    return error_str
 
 
 @app.route('/')
@@ -102,80 +158,24 @@ def flush_artifacts():
     return '', 200
 
 
-@app.route('/roni', methods=['POST'])
-def roni():
-
-    if not preprocess_request():
-        return '', 400
-
-    return '', 200
+@app.route('/validate/project', methods=['POST'])
+def validate_project():
+    return _validate_object('project')
 
 
 @app.route('/validate/package', methods=['POST'])
 def validate_package():
-
-    path = preprocess_request()
-    if not path:
-        return 'Bad request body', 400
-
-    log.info("Validating package '{0}'".format(path))
-
-    key = get_resource_key(path)
-
-    log.debug("MD5 hash: '{0}'".format(key))
-    resource = get_resource(key)
-    if resource:
-        log.debug("Returning cached result for '{0}'".format(key))
-        return resource
-
-    syntax = (False if 'syntax' not in request.form
-              else eval(request.form['syntax']))
-    integrity = (False if 'integrity' not in request.form
-                 else eval(request.form['integrity']))
-    topology = (False if 'topology' not in request.form
-                else eval(request.form['topology']))
-
-    validator = Validator()
-    validator.configure(syntax=syntax, integrity=integrity,
-                        topology=topology, debug=app.debug)
-    result = validator.validate_package(path)
-    print_result(validator, result)
-    json_result = generate_result(validator)
-    set_resource(key, json_result)
-
-    return json_result
+    return _validate_object('package')
 
 
 @app.route('/validate/service', methods=['POST'])
 def validate_service():
-
-    file = request.files['service']
-    filepath = get_file(file)
-
-    validator = Validator()
-    validator.configure(syntax=True, integrity=False,
-                        topology=False, debug=app.debug)
-    result = validator.validate_service(filepath)
-    print_result(validator, result)
-    remove_file(filepath)
-
-    return generate_result(validator)
+    return _validate_object('service')
 
 
 @app.route('/validate/function', methods=['POST'])
 def validate_function():
-
-    file = request.files['function']
-    filepath = get_file(file)
-
-    validator = Validator()
-    validator.configure(syntax=True, integrity=False,
-                        topology=False, debug=app.debug)
-    result = validator.validate_function(filepath)
-    print_result(validator, result)
-    remove_file(filepath)
-
-    return generate_result(validator)
+    return _validate_object('function')
 
 
 def generate_result(validator):
@@ -210,6 +210,7 @@ def get_local(path):
             for f in files:
                 set_artifact(os.path.join(root, f))
     else:
+        req_errors.append("Invalid local path: '{0}'".format(path))
         log.error("Invalid local path: '{0}'".format(path))
         return
 
@@ -247,9 +248,7 @@ def remove_file(filepath):
 
 @atexit.register
 def remove_artifacts():
-
     log.info("Removing artifacts")
-
     for artifact in cache.get('artifacts')[::-1]:
         try:
             os.remove(artifact) if os.path.isfile(artifact) \
