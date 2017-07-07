6713c3c141a64ab44b7ebd9d4118db8583c23b70
Luís Conceição <lconceicao@ubiwhere.com>
2017-07-07 15:59:02 +0100

    workspace bug fix in default service platform id

--- src/son/workspace/workspace.py
+++ src/son/workspace/workspace.py
@@ -293,6 +293,7 @@ class Workspace:
 
     def get_service_platform(self, sp_id):
         if sp_id not in self.service_platforms.keys():
+            print("nah")
             return
         return self.service_platforms[sp_id]
 
